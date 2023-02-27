f7e9f8a48ef83bc1616d128f7b78471eb075b649
GS-M <litvan2001@mail.ru>
2023-02-28 00:04:37 +0300

    test thunk userReduxer

--- src/redux/usersReduser.Thunks.test.ts
+++ src/redux/usersReduser.Thunks.test.ts
@@ -1,23 +1,38 @@
 import { DefaultResponceType, ResultCodeEnum } from "../api/api"
 import { usersAPI } from "../api/users-api"
-import { followTC, actions } from "./usersReduser"
+import { followTC, actions, unfollowTC } from "./usersReduser"
+
 jest.mock("../api/users-api")
-const usersAPIMock = usersAPI
+const usersAPIMock = usersAPI as jest.Mocked<typeof usersAPI>
 const result: DefaultResponceType = {
     data: {},
     resultCode: ResultCodeEnum.Success,
     messages: []
 }
-//@ts-ignore
-usersAPIMock.followUser.mockReturnValue(Promise.resolve(result))
 
-test('', async () => {
+usersAPIMock.followUser.mockReturnValue(Promise.resolve(result))
+usersAPIMock.unfollowUser.mockReturnValue(Promise.resolve(result))
+
+test('follow thunk', async () => {
     const thunk = followTC(1)
     const dispatchMock = jest.fn()
-    //@ts-ignore
-    await thunk(dispatchMock)
+    const getStateMock = jest.fn()
+
+    await thunk(dispatchMock, getStateMock, {})
     expect(dispatchMock).toBeCalledTimes(3)
-    expect(dispatchMock).toHaveBeenLastCalledWith(1, actions.toggleIsFolowingProgressAC(true, 3))
-    expect(dispatchMock).toHaveBeenLastCalledWith(1, actions.followAC(3))
-    expect(dispatchMock).toHaveBeenLastCalledWith(1, actions.toggleIsFolowingProgressAC(false, 3))
+    expect(dispatchMock).toHaveBeenLastCalledWith(1, actions.toggleIsFolowingProgressAC(true, 1))
+    expect(dispatchMock).toHaveBeenLastCalledWith(2, actions.followAC(1))
+    expect(dispatchMock).toHaveBeenLastCalledWith(3, actions.toggleIsFolowingProgressAC(false, 1))
+})
+
+test('follow thunk', async () => {
+    const thunk = unfollowTC(1)
+    const dispatchMock = jest.fn()
+    const getStateMock = jest.fn()
+
+    await thunk(dispatchMock, getStateMock, {})
+    expect(dispatchMock).toBeCalledTimes(3)
+    expect(dispatchMock).toHaveBeenLastCalledWith(1, actions.toggleIsFolowingProgressAC(true, 1))
+    expect(dispatchMock).toHaveBeenLastCalledWith(2, actions.unfollowAC(1))
+    expect(dispatchMock).toHaveBeenLastCalledWith(3, actions.toggleIsFolowingProgressAC(false, 1))
 })
