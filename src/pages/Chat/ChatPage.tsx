3951f89eb8547018450a2eef92a8a6e9bb1ba54a
GS-M <litvan2001@mail.ru>
2023-03-02 20:16:06 +0300

    WebSocket
    Зачистка подписки

--- src/pages/Chat/ChatPage.tsx
+++ src/pages/Chat/ChatPage.tsx
@@ -1,7 +1,6 @@
 import cs from './ChatPage.module.css'
 import { useEffect, useState } from 'react';
 
-const wsChanal = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
 
 export type ChatMessageType = {
     message: string
@@ -19,26 +18,67 @@ const ChatPage: React.FC = () => {
     )
 }
 
-const Chat: React.FC = () => {
 
+
+const Chat: React.FC = () => {
+    const [wsChanal, setWsChanal] = useState<WebSocket | null>(null)
+
+    useEffect(() => {
+        let ws: WebSocket
+        const closeHendler = () => {
+            setTimeout(createChanel, 3000)
+        }
+        function createChanel() {
+            if (ws) {
+                ws.removeEventListener('close', closeHendler) // Зачищаем подписку
+                ws.close()
+            }
+            ws = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
+            if (ws) {
+                ws.addEventListener('close', closeHendler)
+            }
+            setWsChanal(ws)
+        }
+        createChanel()
+
+        return () => {
+            // Когда компонента умрет
+            ws.removeEventListener('close', closeHendler)
+            ws.close()
+        }
+    }, [])
+
+    useEffect(() => {
+
+    }, [wsChanal])
 
     return (
         <div>
-            <ChatMessages />
-            <AddChatMessageForm />
+            <ChatMessages wsChanal={wsChanal} />
+            <AddChatMessageForm wsChanal={wsChanal} />
         </div>
     )
 }
 
-const ChatMessages: React.FC = () => {
+
+
+const ChatMessages: React.FC<{ wsChanal: WebSocket | null }> = ({ wsChanal }) => {
     const [messages, setMessages] = useState<Array<ChatMessageType>>([])
 
+    let messageHendler = (e: MessageEvent) => {
+        let newMessages = JSON.parse(e.data)
+        setMessages((prevMessages) => [...prevMessages, ...newMessages])
+    }
     useEffect(() => {
-        wsChanal.addEventListener('message', (e: MessageEvent) => {
-            let newMessages = JSON.parse(e.data)
-            setMessages((prevMessages) => [...prevMessages, ...newMessages])
-        })
-    }, [])
+        if (wsChanal) {
+            wsChanal.addEventListener('message', messageHendler)
+        } // Синхронизация
+        return () => {
+            if (wsChanal) {
+                wsChanal.removeEventListener('message', messageHendler)
+            } // Рассинхронизация. Зачистка перед тем как придет 2-ой useEffect
+        }
+    }, [wsChanal])
 
     return (
         <div className={cs.chat_body}>
@@ -47,10 +87,12 @@ const ChatMessages: React.FC = () => {
     )
 }
 
+
+
 const Message: React.FC<{ message: ChatMessageType }> = ({ message }) => {
     return (
         <div>
-            <img className={cs.small_avatar} src={message.photo} />
+            <img className={cs.small_avatar} src={message.photo} alt='avatar' />
             <b>{message.userName}</b>
             <br />
             {message.message}
@@ -58,12 +100,28 @@ const Message: React.FC<{ message: ChatMessageType }> = ({ message }) => {
     )
 }
 
-const AddChatMessageForm: React.FC = () => {
+const AddChatMessageForm: React.FC<{ wsChanal: WebSocket | null }> = ({ wsChanal }) => {
     const [message, setMessage] = useState('')
+    const [isReady, setIsReady] = useState<'pending' | 'ready'>('pending')
+
     const sendMessage = () => {
-        wsChanal.send(message)
-        setMessage('')
+        if (wsChanal) {
+            wsChanal.send(message)
+            setMessage('')
+        }
     }
+
+    useEffect(() => {
+        let openHandler = () => {
+            setIsReady('ready')
+        }
+        if (wsChanal) {
+            wsChanal.addEventListener('open', openHandler)
+            return () => {
+                wsChanal.removeEventListener('open', openHandler)
+            }
+        }
+    }, [wsChanal])
 
     return (
         <div>
@@ -71,7 +129,7 @@ const AddChatMessageForm: React.FC = () => {
                 <textarea onChange={(e) => setMessage(e.currentTarget.value)} value={message}></textarea>
             </div>
             <div>
-                <button onClick={sendMessage}>Send</button>
+                <button disabled={isReady !== 'ready'} onClick={sendMessage}>Send</button>
             </div>
         </div>
     )
